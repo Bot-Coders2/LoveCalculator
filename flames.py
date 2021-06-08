e06b57c0a1e6fdf1950a81f70cc718c888f38e82
Vivek Kancharla <65432144+Vivek41916@users.noreply.github.com>
2021-06-08 21:14:31 +0530

    Add files via upload
    to find a relation

---
+++ flames.py
@@ -0,0 +1,14 @@
+s1=(input().upper())
+s2=(input().upper())
+p=len(s1)+len(s2)
+l=['F','L','A','M','E','S']
+for i in range(len(s1)):
+    c=s1[i]
+    if(c in s2):
+        s1=s1.replace(c,'*',1)
+        s2=s2.replace(c,'*',1)
+        p-=2
+while(len(l)!=1):
+    e=(l[(p-1)%(len(l))])
+    l=l[(p-1)%(len(l))+1:]+l[:(p-1)%(len(l))]
+print(l[0])
